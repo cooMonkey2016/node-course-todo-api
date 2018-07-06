8632dc228e64bd2dcbc2e30e4b1f659481f28e17
lijin <coolagain2006@gmail.com>
2018-07-06 15:58:36 +0800

    modify server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -33,6 +34,23 @@ app.get('/todos',(req,res) => {
     },(e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id',(req,res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todos) => {
+        if (!todos) {
+            return res.status(404).send('todos is empty');
+        }
+        res.send({todos});
+    },(e) => {
+        res.status(400).send(e);
+    });
+    //res.send(req.params);
 });
 
 app.listen(3000,() => {
