92dd54f5b0456d2c2835877d29e4430291d04379
lijin <coolagain2006@gmail.com>
2018-07-09 15:35:27 +0800

    Modify files

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: false}));
 
@@ -53,8 +54,8 @@ app.get('/todos/:id',(req,res) => {
     //res.send(req.params);
 });
 
-app.listen(3000,() => {
-    console.log('Started on port 3000');
+app.listen(port,() => {
+    console.log('Started on port ',port);
 });
 
 module.exports = {app};
