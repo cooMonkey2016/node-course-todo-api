d5be91274ca4139968995cab431ab593d0ec3f73
lijin <coolagain2006@gmail.com>
2018-07-16 19:01:55 +0800

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -122,7 +122,18 @@ app.get('/users/me',authenticate,(req,res) => {
     res.send(req.user);
 });
 
-
+app.post('/users/login',(req,res) => {
+    var body = _.pick(req.body,['email','password']);
+    //res.send(body);
+    User.findByCredentials(body.email,body.password).then((user) => {
+        //res.send(user);
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+})
 
 
 app.listen(port,() => {
